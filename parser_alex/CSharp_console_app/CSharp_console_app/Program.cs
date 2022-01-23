08c9456f1fc2767ba41d00ab3a73bc37a39f6d9b
Alex Uicoaba <alexandru.uicoaba@student.upt.ro>
2022-01-23 16:09:18 +0200

    added rdf created in C# from MySQL, added exports folder, where the exported rdf will be located

--- parser_alex/CSharp_console_app/CSharp_console_app/Program.cs
+++ parser_alex/CSharp_console_app/CSharp_console_app/Program.cs
@@ -1,6 +1,31 @@
 ﻿using MySql.Data.MySqlClient;
 namespace HelloWorld
 {
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Project> Movies { get; set; }
+    }
+
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectDescription { get; set; }
+        public string ProjectMaker { get; set; }
+        public List<Person> Actors { get; set; }
+    }
+
+    public class ProjectActor
+    {
+        public int ProjectId { get; set; }
+        public int PersonId { get; set; }
+    }
+
+
     public class Program
     {
         public static string ConnectionString = @"server=localhost;userid=root;password=LMTpass2019*;database=sakila;";
@@ -11,8 +36,6 @@ namespace HelloWorld
             MySqlConnection conn = new MySqlConnection(ConnectionString);
             conn.Open();
 
-            // print the status of connection
-            Console.WriteLine($"MySQL version : {conn.ServerVersion}");
             return conn;
         }
 
@@ -29,10 +52,149 @@ namespace HelloWorld
                         rdr.GetString(2));
             }
         }
+
+        public static List<Person> SelectFromTableInModelPerson(string table)
+        {
+            MySqlConnection localConn = OpenConnection();
+            var statement = "Select * from " + table + ";";
+            var command = new MySqlCommand(statement, localConn);
+            using MySqlDataReader rdr = command.ExecuteReader();
+                List<Person> list = new List<Person>();
+                while (rdr.Read())
+                {
+                    Person person = new Person();
+                    person.PersonId = rdr.GetInt32(0);
+                    person.Name = rdr.GetString(1) + " " + rdr.GetString(2);
+                    person.FirstName = rdr.GetString(1);
+                    person.LastName = rdr.GetString(2);
+                    list.Add(person);
+                }
+                return list;
+            }
+
+          
+
+        public static List<Project> SelectFromTableInModelProject(string table)
+        {
+            MySqlConnection localConn = OpenConnection();
+            var statement = "Select * from " + table + ";";
+            var command = new MySqlCommand(statement, localConn);
+            using MySqlDataReader rdr = command.ExecuteReader();
+
+
+                List<Project> list = new List<Project>();
+                while (rdr.Read())
+                {
+                    Project project = new Project();
+                    project.ProjectId = rdr.GetInt32(0);
+                    project.ProjectName = rdr.GetString(1);
+                    project.ProjectDescription = rdr.GetString(2);
+                    list.Add(project);
+                }
+            return list;    
+        }
+        
+
+        public static List<ProjectActor> SelectFromTableInModelProjectPerson(string table)
+        {
+            MySqlConnection localConn = OpenConnection();
+            var statement = "Select * from " + table + ";";
+            var command = new MySqlCommand(statement, localConn);
+            using MySqlDataReader rdr = command.ExecuteReader();
+         
+                List<ProjectActor> list = new List<ProjectActor>();
+                while (rdr.Read())
+                {
+                    ProjectActor projectactor = new ProjectActor();
+                    projectactor.ProjectId = rdr.GetInt32(0);
+                    projectactor.PersonId = rdr.GetInt32(1);
+                    list.Add(projectactor);
+                }
+            return list;
+            }
+        public static void CreateRdfFile(List<Person> actors, List<Project> movies, List<ProjectActor> moviesActors)
+        {
+            string path = @"C:\Poli\Master\Anu2\MCSW\proiect\mcsw-project\exports\rdf_generated.rdf";
+            File.Delete(path);
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("<?xml version=\"1.0\" encoding=\"utf-8\" ?>");
+                    sw.WriteLine("<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"");
+                    sw.WriteLine("\t \t xmlns:foaf=\"http://xmlns.com/foaf/0.1/\">");
+                    sw.WriteLine("\n");
+                    foreach(var actor in actors)
+                    {
+                        sw.WriteLine("  <rdf:Description rdf:about=\"http://ti.etcti.upt.ro/actors/" + actor.FirstName.ToLower() + "-" +actor.LastName.ToLower() + "\">");
+                        sw.WriteLine("    <rdf:type rdf:resource=\"http://xmlns.com/foaf/0.1/Person\"/>");
+                        sw.WriteLine("    <foaf:givenname>" + actor.FirstName + "</foaf:givenname>");
+                        sw.WriteLine("    <foaf:family_name>" + actor.LastName +"</foaf:family_name>");
+                        sw.WriteLine("  </rdf:Description>");
+                    }
+
+                    sw.WriteLine("\n");
+                    foreach (var movie in movies)
+                    {
+                        sw.WriteLine("  <rdf:Description rdf:about=\"http://ti.etcti.upt.ro/movies/" + movie.ProjectName.Replace(" ", "-") + "\">");
+                        sw.WriteLine("    <rdf:type rdf:resource=\"http://xmlns.com/foaf/0.1/Project\"/>");
+                        sw.WriteLine("    <foaf:title>" + movie.ProjectName + "</foaf:title>");
+                        sw.WriteLine("    <foaf:description>" + movie.ProjectDescription + "</foaf:description>");
+                        if(!(movie.Actors is null || movie.Actors.Count == 0))
+                        {
+                            foreach (var actor in movie.Actors)
+                            {
+                                sw.WriteLine("<foaf:contributors rdf:resource=\"http://ti.etcti.upt.ro/actors/"+ actor.FirstName.ToLower() + "-" + actor.LastName.ToLower() +"\"/>");
+                            }
+                        }
+                        sw.WriteLine("  </rdf:Description>");
+                        
+                    }
+                    sw.WriteLine("</rdf:RDF>");
+
+                }
+            }
+        }
+
         static void Main()
         {
-            SelectFromTable("language");
-            Console.WriteLine("Hello World!");
+            var actors = SelectFromTableInModelPerson("actor");
+            var movies = SelectFromTableInModelProject("film");
+            var moviesActors = SelectFromTableInModelProjectPerson("film_actor");
+
+             foreach (var actor in actors)
+                {
+                List<Project> mov = new List<Project>();
+                foreach (var movie in movies)
+                {
+                    foreach(var ma in moviesActors)
+                    {
+                        if(actor.PersonId == ma.PersonId && movie.ProjectId == ma.ProjectId)
+                        {
+                            mov.Add(movie);
+                        }
+                    }
+                }
+                actor.Movies = mov;
+             }
+
+            foreach (var movie in movies)
+            {
+                List<Person> per = new List<Person>();
+                foreach (var actor in actors)
+                {
+                    foreach (var ma in moviesActors)
+                    {
+                        if (actor.PersonId == ma.PersonId && movie.ProjectId == ma.ProjectId)
+                        {
+                            per.Add(actor);
+                        }
+                    }
+                }
+                movie.Actors = per;
+            }
+            CreateRdfFile(actors,movies,moviesActors);
         }
     }
 }
